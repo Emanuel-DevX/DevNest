1968a1a61d56dd68661a12510003aa3052f9e1cf
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-14 22:40:37 -0700

    Required callback function added inside passport.use

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,10 +1,18 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALL_BACK_URL,
-  })
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALL_BACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // This callback is required!
+      // You can process user data here, or just pass it along
+      return done(null, profile);
+    }
+  )
 );
--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const passport = require("passport");
+require("./auth/google"); // Load Google Strategy
+
+
 const app = express();
 dotenv.config();
-
 const PORT = process.env.PORT;
 const API_URL = process.env.API_URL;
 
-app.get("/",(req, res)=>{
-    res.send("Welcome to DevNest")
-})
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(passport.initialize());
+
+
+const authRoutes = require("./routes/auth");
+
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to DevNest");
+});
 
 app.listen(PORT, () => {
   console.log(`All good server is running on ${API_URL}`);
