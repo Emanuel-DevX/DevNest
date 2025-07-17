126da72e02995544f13ee4678792e3fc77dcb004
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-17 13:57:07 -0700

    Define Task schema model in mongoose

---
+++ server/models/Task.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    duration: {
+      type: Number,
+      default: 30,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+    },
+    sprintId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Sprint",
+      required: true,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const taskModel = mongoose.model("Task", taskSchema);
+module.exports = taskModel;
