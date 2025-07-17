07ae26fae9c7472efbaa107c5d3aa7c0a6e1a8b9
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-17 14:10:39 -0700

    Add membership schema model for project collaborations

--- server/models/Sprint.js
+++ server/models/Sprint.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
-const sprintSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 20,
-    trim: true,
+const sprintSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+    },
+    startDate: Date,
+    endDate: Date,
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
   },
-  startDate: Date,
-  endDate: Date,
-  projectId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Project",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const sprintModel = mongoose.model("Sprint", sprintSchema);
 module.exports = sprintModel;
