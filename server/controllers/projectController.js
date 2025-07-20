33c538882d89ab0baa8ee8c2e6f9249d588d33e8
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-20 12:36:24 -0700

    Define POST request handler for project creation

---
+++ server/controllers/projectController.js
@@ -0,0 +1,23 @@
+const Project = require("../models/Project");
+const createProject = async (req, res) => {
+  const { name, description } = req.body;
+  if (!name.trim() || name.trim().length < 3) {
+    return res
+      .status(401)
+      .json({ error: "Project names are required to be 3 or more charactes" });
+  }
+  try {
+    await Project.create({
+      name: name.trim(),
+      description: description,
+      owner: req.user._id,
+    });
+    return res.status(201).json({ message: "Project created successfully!" });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Could not create project", error: err });
+  }
+};
+
+module.exports = { createProject };
