48244b888e805b56ff87aa18a6fdf0069334c0b5
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-21 15:31:38 -0700

    Test dasboard project list rendering

--- client/src/pages/dashboard/Dasboard.jsx
+++ client/src/pages/dashboard/Dasboard.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 import { isAuthenticated, getCurrentUser } from "../../lib/auth";
 import fetcher from "../../lib/api";
 import { useState } from "react";
-
 import Overview from "./Overview";
-
+import ProjectList from "./ProjectList";
 export default function Dashboard() {
   const [overview, setOverview] = useState({});
+  const [projectList, setProjectList] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,11 +17,14 @@ export default function Dashboard() {
   }, []);
   useEffect(() => {
     const fetchData = async () => {
-      const endpoint = "/dashboard/overview";
+      let endpoint = "/dashboard/overview";
       const res = await fetcher(endpoint);
       const user = getCurrentUser();
       res.user = user;
       setOverview(res);
+
+      const projects = await fetcher("/projects");
+      setProjectList(projects);
     };
     fetchData();
   }, []);
@@ -30,9 +33,11 @@ export default function Dashboard() {
     <>
       <div className="min-h-screen flex flex-col">
         <Overview userData={overview} />
-        <h1 className="text-2xl font-jetbrains text-teal-300"><span className="text-teal-400 mr-0.5">#</span>Projects</h1>
+        <h1 className="text-2xl mb-2 font-jetbrains text-teal-300">
+          <span className="text-teal-400 mr-0.5">#</span>Projects
+        </h1>
+        <ProjectList projectList={projectList} />
       </div>
-
     </>
   );
 }
