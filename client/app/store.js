b4a798d2ae0aa585547de7542ee0903621046be6
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-21 18:57:44 -0700

    Configure redux store

---
+++ client/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import projectReducer from "./features/projectSlice";
+export const store = configureStore({
+  reducer: {
+    project: projectReducer,
+  },
+});
