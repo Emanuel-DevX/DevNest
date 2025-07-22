ed84fdd0f815798e8a678031d65da1852078fcb2
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-21 21:31:57 -0700

    Fix sidebar project rendering issues on refresh

--- client/src/components/ProjectBar.jsx
+++ client/src/components/ProjectBar.jsx
@@ -1,14 +1,26 @@
-import { useSelector } from "react-redux";
-import { getSortedProjects } from "../features/projectSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getSortedProjects, setProjects } from "../features/projectSlice";
 import LetterIcon from "./LetterIcon";
 import { Link } from "react-router-dom";
+import fetcher from "../lib/api";
 
 const ProjectBar = function ({ collapsed }) {
   const sortedProjects = useSelector(getSortedProjects);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchData = async () => {
+      const projects = await fetcher("/projects");
+      dispatch(setProjects(projects));
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       {sortedProjects.map((project) => (
-        <Link to=  {`project/${project._id}`} key={project._id}>
+        <Link to={`project/${project._id}`} key={project._id}>
           <div className="flex gap-2 max-h-8 overflow-hidden my-2 items-center">
             <LetterIcon letter={project.name[0]} size="sm" />
             {!collapsed ? (
