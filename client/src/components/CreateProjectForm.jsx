b70972f14e83970a5e72dec4d876a99e6404fe3f
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-29 11:26:00 -0700

    Complete create a project form component

--- client/src/components/CreateProjectForm.jsx
+++ client/src/components/CreateProjectForm.jsx
@@ -1,11 +1,51 @@
-const CreateProjectForm = function ({ onCancel, onSave }) {
+import { useState } from "react";
+
+const CreateProjectForm = function ({ onCancel }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const handleCreateProject = () => {};
   return (
     <>
-      <div className="fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center">
-        You will need to enter project date here
+      <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/80 flex justify-center items-center">
+        <div className="w-sm bg-zinc-900  rounded-xl p-4 min-w-sm flex flex-col gap-3">
+          <div className="mx-auto text-xl text-teal-500 font-bold">
+            Create a Project
+          </div>
+
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Name"
+            className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-teal-500 focus:border-teal-500 transition"
+          />
+
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description"
+            rows={3}
+            className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-teal-500 focus:border-teal-500 transition resize-none"
+          />
+
+          <div className="w-full flex justify-end gap-3 pt-2">
+            <button
+              onClick={onCancel}
+              className="px-5 py-1 bg-zinc-700 hover:bg-zinc-600 rounded-2xl"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={handleCreateProject}
+              className="px-7 py-1 bg-teal-700 rounded-2xl font-bold hover:bg-teal-600"
+            >
+              Save
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
-export default CreateProjectForm
+export default CreateProjectForm;
