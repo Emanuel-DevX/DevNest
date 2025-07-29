3558275528536c9d98fed574d582092e688975f3
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-29 11:01:06 -0700

    Declare CreateProjectForm component with placeholder

---
+++ client/src/components/CreateProjectForm.jsx
@@ -0,0 +1,11 @@
+const CreateProjectForm = function ({ onCancel, onSave }) {
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center">
+        You will need to enter project date here
+      </div>
+    </>
+  );
+};
+
+export default CreateProjectForm
