19dd2796f0cc516a605ae291d899144ed0fd8e93
Emanuel-DevX <jefymoxi@gmail.com>
2025-08-06 22:35:22 -0700

    Create Note schema model

---
+++ server/models/Note.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, default: "" }, // markdown content
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    projectId: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Note", noteSchema);
