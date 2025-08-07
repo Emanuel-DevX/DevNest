c200b44971404343d8107522d086e038a4a754a7
Emanuel-DevX <jefymoxi@gmail.com>
2025-08-07 14:14:54 -0700

    Create NoteCard component

--- client/src/pages/notes/NotesLayout.jsx
+++ client/src/pages/notes/NotesLayout.jsx
@@ -4,30 +4,20 @@ import { useEffect, useState, useMemo, useCallback } from "react";
 import fetcher from "../../lib/api";
 
 export default function NotesLayout() {
-  const { projectId } = useParams();
-
-  const [notes, setNotes] = useState([]);
-  const [isLoading, setLoading] = useState(true);
+    const [notes, setNotes] = useState([]);
   const [error, setError] = useState(null);
 
-  const basePath = useMemo(
-    () => (projectId ? `/project/${projectId}/notes` : `/notes`),
-    [projectId]
-  );
 
   const fetchNotes = useCallback(async () => {
     try {
-      setLoading(true);
       setError(null);
-      const url =`/notes`;
+      const url = `/notes`;
       const data = await fetcher(url);
       setNotes(data || []);
     } catch (err) {
       setError(err?.message || "Failed to load notes");
-    } finally {
-      setLoading(false);
-    }
-  }, [projectId]);
+    } 
+  }, []);
 
   useEffect(() => {
     fetchNotes();
@@ -38,18 +28,20 @@ export default function NotesLayout() {
     () => ({
       notes,
       refresh: fetchNotes,
-  
     }),
-    [notes, fetchNotes, ]
+    [notes, fetchNotes]
   );
 
   return (
     <div className=" space-y-4">
       <header className="flex items-center justify-between">
-        <h1 className="text-xl font-semibold text-teal-400">
-          My Notes
-        </h1>
-        <Link to={`${basePath}/new`} className="px-3 py-1 rounded bg-teal-600 text-white">+ New</Link>
+        <h1 className="text-xl font-semibold text-teal-400">My Notes</h1>
+        <Link
+          to={`/notes/new`}
+          className="px-3 py-1 rounded bg-teal-600 text-white"
+        >
+          + New
+        </Link>
       </header>
 
       <Outlet context={ctx} />
@@ -58,7 +50,6 @@ export default function NotesLayout() {
           {error}
         </div>
       )}
-
     </div>
   );
 }
--- client/src/pages/notes/NotesList.jsx
+++ client/src/pages/notes/NotesList.jsx
@@ -1,4 +1,26 @@
+import { useOutletContext, useParams } from "react-router-dom";
+import NoteCard from "./NoteCard";
+import { useMemo } from "react";
+
 const NotesList = function () {
-  return <>Notes List</>;
+  const { projectId } = useParams();
+
+  const basePath = useMemo(
+    () => (projectId ? `/project/${projectId}/notes` : `/notes`),
+    [projectId]
+  );
+  const { refresh, notes } = useOutletContext();
+  console.log(notes);
+  return (
+    <>
+      <div className="flex flex-col gap-2 md:flex-row w-full flex-wrap justify-center">
+        {notes.map((note) => (
+          <div key={note._id} className="w-full md:w-[48%] lg:w-[32%]">
+            <NoteCard basePath={basePath} note={note} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 export default NotesList;
